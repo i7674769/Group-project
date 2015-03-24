f79c24453893f96bb90a34e4c1c607f5d298d4fc
Darlene <i7677264@bournemouth.ac.uk>
2015-03-24 21:12:48 +0000

    Update edit_post.php
    slightly changed, but still not working

--- public/edit_post.php
+++ public/edit_post.php
@@ -5,20 +5,24 @@
 <?php if(isset($_SESSION["user"])) {?>
     <form>
         New post: <input type="text" name="new_tweet" value="" />
-        <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="new_post" value="Submit" />
     </form>
-<?php } ?>
+
 
 <?php
-    if(isset($_POST["signup"])
-      or (isset($_GET["id"]))
-      ) {
-        $userID = $_GET["id"]; 
-        $new_tweet = $_POST["new_tweet"];
-        
-        $query = "UPDATE posts SET tweet='$new_tweet' WHERE user_id = '{$userID}'";
-        $result = mysqli_query($connect, $query); 
-      }
-$new_tweet = "";
+    if(isset($_GET["id"])){
+        $postID = $_GET["id"]; 
+            
+            if (isset($_POST["new_post"])){
+                $new_tweet = $_POST["new_tweet"];
 
+                $query = "DELETE FROM posts WHERE id = '{$postID}' and user_id = '{$_SESSION['user_id']}'";
+                $result = mysqli_query($connect, $query); 
+            } else {
+                echo "This is not working, Darlene!";
+            }
+        $new_tweet = "";
+        }
 ?>
+<?php } ?>
+
