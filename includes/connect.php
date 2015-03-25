7b125be4d6a632c47d6dd74fa774e4ff2f3585d2
Darlene <i7677264@bournemouth.ac.uk>
2015-03-25 20:23:14 +0000

    Update and rename public/connect.php to includes/connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
     define("DB_SERVER", "localhost");
-    define("DB_USER", "becky"); 
-    define("DB_PASS", "password"); 
-    define("DB_NAME", "becky"); 
+    define("DB_USER", "root"); 
+    define("DB_PASS", "password"); //9WPq7dKgU+U=
+    define("DB_NAME", "test2"); 
 
     $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
