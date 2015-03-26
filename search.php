980e74c0eb970be367ab0173c82ef7e25a0db889
Darlene <i7677264@bournemouth.ac.uk>
2015-03-26 22:15:28 +0000

    Create search.php
    this is the file for the new page that is just for searching (just the html) :)

---
+++ search.php
@@ -0,0 +1,61 @@
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("signup.php"); ?>
+<?php require_once("post_submit.php"); ?>
+<?php require_once("login.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Format Code</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+        <body class="search_page">
+                   <?php if(isset($_SESSION["user"])) {?>
+                
+                    <div id="nav">
+                            <div class="section1">
+                                <a href="index.php">
+                                    <p>Home</p>
+                                </a>
+                            </div>           
+                            <div class="section2">
+                                <a href="aboutus.php"> 
+                                    <p>About</p>
+                                </a>
+                            </div>
+                        
+                            <div class="section3">
+                                <a href="search.php">
+                                    <p>Search</p>
+                                </a>
+                            </div>
+                    </div>
+                    
+                    <div class="box_tweet_gender">
+                        <div class="box_hello">
+                            <form class= "logout_button" method="link" action="logout.php">
+                            <input type="submit" value="Logout">
+                            </form>
+                        
+                        <div class="delete_toggle">
+                        <p>Delete Account</p>
+                        </div>
+                        
+                        <div class="delete"><a href="delete.php?id=<?php echo $_SESSION["user_id"] ?>">Sure?</a></div>
+                         
+                        </div>
+                        <div class="box_hello"><a>Hello, <?php echo ucfirst($_SESSION["user"]); ?>! </a></div>
+                    </div>
+            
+                    <div>
+                            
+            
+                    </div>
+                    <?php } ?>
+
+    </body>
+    </html>
+<?php include_once("../includes/templates/footer.php"); ?> 
