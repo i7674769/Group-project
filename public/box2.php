92693a7bc429da6d41b733423135d99e0ad7ce4a
Darlene <i7677264@bournemouth.ac.uk>
2015-03-28 18:30:59 +0000

    Update box2.php

--- public/box2.php
+++ public/box2.php
@@ -13,23 +13,13 @@
     
     <div id="place_delete_edit2"> 
         <div class="place_of_post" <p><?php echo "Place: ".ucfirst($row["place_of_post"]); ?></p> </div>
-
+        <div class="gender" <h2><?php echo "by ".ucfirst($row["gender"]); ?></h2> </div>
     <?php if ($_SESSION["user_id"]==$row["user_id"]) {?>
         <div class="delete_edit" id="delete_post"><a href="delete_post.php?id=<?php echo $row["id"]; ?>">Delete post?</a></div>
-        <form class="edit_post" action="edit_post.php" method="post">
-            New post: <input type="text" name="new_tweet" value="" />
-        </form>
-        <div class="delete_edit" id="edit_post"><a href="edit_post.php?id=<?php echo $row["id"]; ?>">Update post!</a></div> 
     <?php } ?>
-
     </div>
-        <div class="gender" <h2><?php echo ucfirst($row["gender"]).":"; ?></h2> </div>
         <div class="tweet" <p><?php echo ucfirst($row["tweet"]); ?></p> </div>
     </div>
-
-
-<!-- This is where the form for the report button is gonna be in case we get it working-->
-
 
 <?php } ?>
         
