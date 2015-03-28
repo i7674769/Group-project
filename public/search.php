46bd6d751787e170bbfbbd7033450c099f59a2ae
becky <becky_hughes94@hotmail.co.uk>
2015-03-28 21:15:30 +0000

    deleted and added
    deleted old search files, should just be the new one

--- public/search.php
+++ public/search.php
@@ -1,30 +1,69 @@
-<<?php
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("signup.php"); ?>
+<?php require_once("post_submit.php"); ?>
+<?php require_once("login.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
 
-$sql = mysql_query("SELECT * FROM post WHERE pagecontent LIKE '%$_GET[term]%' LIMIT 0,$_GET[results]");
+<?php
+$output='';
 
-while($ser = mysql_fetch_array($sql)) { echo "<h2><a href='$ser[pageurl]'>$ser[pageurl]</a></h2>"; }
-
-//URL of page, will get source code read and made into variable
-
-$pagedata=htmlspecialchars(file_get_contents($_POST['url']));
-
-// take away some single quotes that could disorder the query
-$pagedata=str_replace(" ", " ", $pagedata);
-
-//small message to see if its been added
-
-echo"URL Added.<br><a href='./addurl.php>Continue...</a>";?>
-
-
-
-
-
-
+if(isset($_GET["search"])) {
+    $searchq= $_GET["search"];
+   
+    $query = mysql_query("SELECT * FROM posts WHERE firstname LIKE '%{$_GET["$searchq"]}%' OR lastname LIKE '%{$_GET["$searchq"]}%'") or die("could not search!");
+    
+    $count= mysql_num_rows($query);
+    if($count==0){
+        $output = 'There was no search results!';
+    }else{
+            while ($row=mysql_fetch_array($query)) {
+                $id = $row['id'];
+                $user_id =$row['user_id'];
+                $tweet =$row['tweet'];
+                $gender =$row['gender'];
+                $dateofpost =$row['dateofpost'];
+                $placeofpost =$row['placeofpost'];
+                
+                $output .='<div>'.$id.''.$user_id.''.$tweet.''.$gender.''.$dateofpost.''.$placeofpost.'</div>';
+    }
+    }
+}
 
 
 
 
 
 ?>
-<hr>
-<a href="./index.php">Go Back</a>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <title>Search</title>
+    
+    </head>
+    
+<body>
+
+
+
+<form action="index.php" method="post">
+    
+    <input type="text" name="search" placeholder="Search for posts..."/>
+           <input type="submit" value=">>" />
+    
+    
+    
+    
+    </form>
+    <?php print("$output");?>
+
+
+
+    
+    
+</body>
+</html>
