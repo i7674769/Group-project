fd39070a6ba5da89368b84e3940d8102fd7d1222
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 14:49:48 +0100

    Update post_submit.php
    took out blank spaces

--- public/post_submit.php
+++ public/post_submit.php
@@ -12,22 +12,22 @@
         if(empty($post)) {
             echo '<script type="text/javascript" language="Javascript">  
             alert("Please enter a post!") 
-            </script>'; // If the post field is empty, this message will be echoed out using js.
+            </script>'; //If the post field is empty, this message will be echoed out using js.
         }else if (empty($place_of_post)) {
             echo '<script type="text/javascript" language="Javascript">  
             alert("Please enter a place!") 
-            </script>'; // If the place_of_post field is empty, this message will be echoed out using js.
+            </script>'; //If the place_of_post field is empty, this message will be echoed out using js.
         }else {
             $query = "INSERT INTO posts (post, user_id, gender, place_of_post) VALUES ('{$post}', '{$_SESSION['user_id']}', '{$_SESSION['gender']}', '{$place_of_post}')"; //Query to save the entered data in a collum into the table 'posts'.
             $result = mysqli_query($connect, $query); 
             if($result) {
                 echo '<script type="text/javascript" language="Javascript">  
                 alert("Your post was added!") 
-                </script>'; // If the post was added, this message will be echoed out using js.
+                </script>'; //If the post was added, this message will be echoed out using js.
             } else {
                 echo '<script type="text/javascript" language="Javascript">  
                 alert("Something went wrong!") 
-                </script>'; // If the post was not added, this message will be echoed out using js.
+                </script>'; //If the post was not added, this message will be echoed out using js.
             }
             
             $post = ""; //In case there is nothing entered, this is empty.
