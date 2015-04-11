5d9c1986d3bd28701bdbd2816f79645f3878ef98
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 11:10:33 +0100

    Update search.php
    added validation :)

--- public/search.php
+++ public/search.php
@@ -1,25 +1,71 @@
-<?php
-    $output='';
-        if(isset($_GET["search"])) {
-            $searchq= $_GET["search"];
-            $query = mysql_query("SELECT * FROM posts WHERE firstname LIKE '%{$_GET["$searchq"]}%' OR lastname LIKE '%{$_GET["$searchq"]}%'") or die("could not search!");
-            $count= mysql_num_rows($query);
-                if ($count==0){
-                    $output = 'There was no search results!';
-                } else {
-                        while ($row=mysql_fetch_array($query)) {
-                            $id = $row['id'];
-                            $user_id =$row['user_id'];
-                            $tweet =$row['tweet'];
-                            $gender =$row['gender'];
-                            $dateofpost =$row['dateofpost'];
-                            $placeofpost =$row['placeofpost'];
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("signup.php"); ?>
+<?php require_once("post_submit.php"); ?>
+<?php require_once("login.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
 
-                            $output .='<div>'.$id.''.$user_id.''.$tweet.''.$gender.''.$dateofpost.''.$placeofpost.'</div>';
-                }
-                }
-                }
+<!doctype html>
+<html>
+    <head>
+        <title>Format Code</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+        <body class="search_page">
+                   <?php if(isset($_SESSION["user"])) {?>
+                
+                    <div id="nav">
+                            <div class="section1">
+                                <a href="index.php">
+                                    <img class="turtle" src="img/turtle01.png">
+                                </a>
+                            </div>           
+                            <div class="section2">
+                                <a href="aboutus.php"> 
+                                    <p>About</p>
+                                </a>
+                            </div>
+                        
+                            <div class="section3">
+                                <a href="search.php">
+                                    <p>Search</p>
+                                </a>
+                            </div>
+                        
+                            <div class="section4">
+                                <a href="filter.php">
+                                    <p>Filter</p>
+                                </a>
+                            </div>
+                    </div>
+                    
+                    <div class="box_tweet_gender">
+                        <div class="box_hello">
+                            <form class= "logout_button" method="link" action="logout.php">
+                            <input type="submit" value="Logout">
+                            </form>
+                        
+                        <div class="delete_toggle">
+                        <p>Delete Account</p>
+                        </div>
+                        
+                        <div class="delete"><a href="delete.php?id=<?php echo $_SESSION["user_id"] ?>">Sure?</a></div>
+                         
+                        </div>
+                        <div class="box_hello"><a>Hello, <?php echo ucfirst($_SESSION["user"]); ?>! </a></div>
+                    </div>
+            
+            
+            
+                    <form class= "search" action="index.php" method="post">
+                        <p>Search</p><input type="text" name="search" placeholder="Search by words..." pattern="[^'\x22]+" title="Invalid input">
+                        <input type="submit" value="Go!" />
+                    </form>
+    
+                    <?php } ?>
 
-<?php print("$output");?>
+    </body>
+</html>
 
-
+<?php include_once("../includes/templates/footer.php"); ?> 
