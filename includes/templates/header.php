f89b97cbcdd8d968705b6a7d959133c064a3a055
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 10:22:19 +0100

    Update header.php
    no more bootstrap

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,15 +1,10 @@
 <!doctype html>
 <html>
-    <head>
+    <head> 
         <title>Project</title>
-        <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-        <link href="css/normal.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
-        <link href="css/styles.css" rel="stylesheet">
-       
+        <link href="../../public/css/normal.css" rel="stylesheet">
+        <link href="../../public/css/style.css" rel="stylesheet" type="text/css">
+        <link href="../../public/css/styles.css" rel="stylesheet" type="text/css">
+        
     </head>
     <body>
