3b5a5a1222404e6533f070ead737de5d41e327ea
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 11:29:01 +0100

    Update signup.php
    Since we are using 'pattern' to validate the input into our form fields, I took out the other form of validation. The password/confirm password had to stay in though because it still has to be checked if they are the same. I also changed the echoed message to an alert message.

--- public/signup.php
+++ public/signup.php
@@ -11,34 +11,29 @@
         $student_id = $_POST['student_id'];
         $gender = $_POST["gender"];
         
-        if(empty($name)) {
-            $message = "Please enter valid name. ";
-            echo '<div class="message">'.$message.'</div>';
-        } else if(empty($password)) {
-            $message = "Please enter valid password. ";
-            echo '<div class="message">'.$message.'</div>';
-        } else if(empty($confirm_password)) {
+        //this checks if the password is the same as the confirm_password
+        //originally used for checking if all the fields were empty but we switched to another form of validation
+        
+        if(empty($confirm_password)) {
             $message = "Please enter password again. ";
             echo '<div class="message">'.$message.'</div>';
         } else if ($password != $confirm_password) {
             $message = "Passwords do not match. ";
             echo '<div class="message">'.$message.'</div>';
-        } else if(empty($student_id)) {
-            $message = "Please enter valid Student ID. ";
-            echo '<div class="message">'.$message.'</div>';
-        } else if(empty($gender)) {
-            $message = "Please enter valid gender. ";
-            echo '<div class="message">'.$message.'</div>';
         } else {
             $query = "INSERT INTO users (name, password, student_id, gender) VALUES ('{$name}', '{$password}', '{$student_id}','{$gender}')";
             $result = mysqli_query($connect, $query); 
-            $message = "Your account has been registered!";
-            echo $message; 
+            
                 if($result) {
-                    $message = "Success, your account was added!";   
+                    echo '<script type="text/javascript" language="Javascript">  
+                    alert("Your account has been registered!") 
+                    </script>'; 
                 } else {
-                    $message = "Sorry, something went wrong!"; 
+                    echo '<script type="text/javascript" language="Javascript">  
+                    alert("Sorry, something went wrong!") 
+                    </script>'; 
                 }
+            
                     $name = "";
                     $password = ""; 
                     $confirm_password = ""; 
