d017c9d820f6b3beaf98b9d125c9b83ed884130a
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 14:34:11 +0100

    Update login.php
    I commented this as well.

--- public/login.php
+++ public/login.php
@@ -1,26 +1,28 @@
+<!--Including several files into the index file using a php function. 'Require once' checks if the file has already been included and in case it has, it will not include it a second time.--> 
 <?php require_once("../includes/sessions.php"); ?>
 <?php require_once("../includes/connect.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 
 <?php     
-    if(isset($_POST["submit"])) {
-        $post = ucfirst($_POST["post"]);
+    if(isset($_POST["submit"])) { //Only if the form 'submit' is submitted.
+        $post = ucfirst($_POST["post"]); //Declaring variable $post. //The first character is displayed capital.
     } else {
-        $post = ""; 
+        $post = ""; //Else field is empty.
     }
-    if(isset($_POST["login"])) {
-        $name = $_POST["name"];
-        $password = $_POST["password"];
-        $query = "SELECT * FROM users WHERE name='{$name}' AND password='{$password}'LIMIT 1";
+    if(isset($_POST["login"])) { //Only if the form 'login' is submitted.
+        $name = $_POST["name"]; //Declaring variable $name.
+        $password = $_POST["password"]; //Declaring variable password.
+        $query = "SELECT * FROM users WHERE name='{$name}' AND password='{$password}'LIMIT 1"; //Query to select name and password from the table users. 
         $result = mysqli_query($connect, $query); 
         
-        if ($user = mysqli_fetch_assoc($result)) {
+        if ($user = mysqli_fetch_assoc($result)) { //Only if successful. Saving variables user/gender/user_id from selected user collum in session. 
             $_SESSION["user"] = $user["name"];
             $_SESSION["gender"] = $user["gender"];
             $_SESSION["user_id"] = $user ["id"];
         } else {
-            $message = "Please enter a valid username and password.";
-            echo '<div class="message">'.$message.'</div>';
+            echo '<script type="text/javascript" language="Javascript">  
+            alert("This user does not exist!") 
+            </script>'; //In case no results found, this message is echoed out in an alert window using js. 
         }
     } 
 ?>
