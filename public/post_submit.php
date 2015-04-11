0dd7464f54120bc8e2d99b9b386b9b29abe5b75e
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 11:43:22 +0100

    Update post_submit.php
    replaced echo with alert

--- public/post_submit.php
+++ public/post_submit.php
@@ -5,10 +5,10 @@
 <?php
     if(isset($_POST["submit"])) {
         
-        $tweet = $_POST['tweet'];
+        $post = $_POST['post'];
         $place_of_post = $_POST['place_of_post'];
         
-        if(empty($tweet)) {
+        if(empty($post)) {
             echo '<script type="text/javascript" language="Javascript">  
             alert("Please enter a post!") 
             </script>'; 
@@ -17,15 +17,19 @@
             alert("Please enter a place!") 
             </script>'; 
         }else {
-            $query = "INSERT INTO posts (tweet, user_id, gender, place_of_post) VALUES ('{$tweet}', '{$_SESSION['user_id']}', '{$_SESSION['gender']}', '{$place_of_post}')"; 
+            $query = "INSERT INTO posts (post, user_id, gender, place_of_post) VALUES ('{$post}', '{$_SESSION['user_id']}', '{$_SESSION['gender']}', '{$place_of_post}')"; 
             $result = mysqli_query($connect, $query); 
             if($result) {
-                $message = "Success, your post was added!";   
+                echo '<script type="text/javascript" language="Javascript">  
+                alert("Your post was added!") 
+                </script>';   
             } else {
-                $message = "Sorry, something went wrong!"; 
+                echo '<script type="text/javascript" language="Javascript">  
+                alert("Something went wrong!") 
+                </script>'; 
             }
             
-            $tweet = "";
+            $post = "";
             $name = ""; 
             $gender = "";
   
