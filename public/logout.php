708ed1ddcd168dccb40c608b506e61d56695096b
Darlene <i7677264@bournemouth.ac.uk>
2015-04-11 14:37:21 +0100

    Update logout.php
    commented

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  session_destroy();
-  header('Location: index.php');
-  exit;
+  session_destroy(); //Session is being destroyed. 
+  header('Location: index.php'); //User is redirected to index.php. Because the session is destroyed, the if-statement in index.php will be wrong -> will not be redirected to the post_page but the signup/login_page.
+  exit; //Exit logout.php. 
 ?>
