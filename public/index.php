7bfb75aaafbac78a4014815b9d5083fef4c5afaf
Darlene <i7677264@bournemouth.ac.uk>
2015-04-12 11:52:02 +0100

    Update index.php
    now the backgrounds work :)

--- public/index.php
+++ public/index.php
@@ -40,14 +40,12 @@
         <link href="css/style.css" rel="stylesheet" type="text/css">
         <link href="css/styles.css" rel="stylesheet" type="text/css">
     </head>
-    
-        <body class="login_page">
             <?php if(isset($_SESSION["user"])){ //An if-statement is used here to make sure the following content is only displayed once the session has started. This seperated the login_page and the sign_up page. ?>
-                
+                <body class="post_page">
                     <div id="nav">
                         <a href="">
                             <div class="section1">
-                                    <img class="turtle" src="img/turtle01.png">
+                                <img class="turtle" src="img/turtle01.png">
                             </div>   
                         </a>
                         <a href="aboutus.php"> 
@@ -66,20 +64,16 @@
                             </div>
                         </a>
                     </div>
-    
-                
+                    
                     <div class="box_post_gender">
                         <div class="box_hello">
                             <form class= "logout_button" method="link" action="logout.php">
                             <input type="submit" value="Logout">
                             </form>
-                        
                         <div class="delete_toggle">
-                        <p>Delete Account</p>
+                            <p>Delete Account</p>
                         </div>
-                        
                         <div class="delete"><a href="delete.php?id=<?php echo $_SESSION["user_id"] ?>">Sure?</a></div>
-                         
                         </div>
                         <div class="box_hello"><a>Hello, <?php echo ucfirst($_SESSION["user"]); //To adress the user logged in, the name saved as session variable is displayed. ?>! </a></div>
                         <form class="box_post_gender_content" action="index.php" method="post">
@@ -96,50 +90,51 @@
                                             <option value="none">None</option>
                                             </select>
                         <input type="submit" name="submit" value="Submit" />
-                        </div>
                         </form>
+                    </div>
+                    
+                    <div class="footer">
+                        Copyright ©Turtletalks.com 
+                    </div>
+                </body>
     
-                        <div class="footer">
-                            Copyright ©Turtletalks.com 
-                        </div>
-                    </body>
-
-            <body class="post_page">
                 <?php } else {?>
-                <!--In case the session was not started, the following content is displayed (login/signup_page).-->
-                        <div class="register-title"></p></div>
-                        <div class="login_box">
-                            <form action="index.php" method="post" autocomplete="off">
-                                <input type="text" name="name" value="" placeholder="Name"/>
-                                <input type="password" name="password" value="" placeholder="Password"/>
-                                <input type="submit" name="login" value="Login" /></p>
-                            </form> </div>
-
-                        <div class="login_toggle">
-                        <p> No account?</p>
-                        </div>
-
-                         <div class="sign_up_box">   
-                                <p>No account? Sign up.</p>
+                    <body class="login_page">
+                    <!--In case the session was not started, the following content is displayed (login/signup_page).-->
+                            <div class="register-title"></p></div>
+                            <div class="login_box">
                                 <form action="index.php" method="post" autocomplete="off">
-                                
-                                <div class="register-switch">
-                                      <input type="radio" name="gender" value="female" id="sex_f" class="register-switch-input" checked>
-                                      <label for="sex_f" class="register-switch-label">Female</label>
-                                      <input type="radio" name="gender" value="male" id="sex_m" class="register-switch-input">
-                                      <label for="sex_m" class="register-switch-label">Male</label>
-                                </div>
-                                <input type="text" name="name" value="" placeholder="Name" pattern="{1,}" required title="Please enter a name."/>
-                                <!-- The following Attribute 'pattern' is used to validate the input.-->
-                                <input type="password" name="password" value="" placeholder="Password" pattern="(?=.*\d).{8,}" required title="Must contain at least one number.">
-                                <input type="password" name="confirm_password" value="" placeholder="Confirm Password"/>
-                                <!-- The following Attribute 'pattern' is used to validate the input.-->
-                                <input type="text" name="student_id" value="" placeholder="Student ID" pattern="[0-9].{6,}" required title="7 characters minimum and only numbers are allowed."/>
-                                <div class="dropdown">    
-                                    <input type="submit" name="signup" value="Sign up!"/> </p> 
-                                </form>
-                        </div>
-                <?php } ?>
+                                    <input type="text" name="name" value="" placeholder="Name"/>
+                                    <input type="password" name="password" value="" placeholder="Password"/>
+                                    <input type="submit" name="login" value="Login" /></p>
+                                </form> </div>
+
+                            <div class="login_toggle">
+                            <p> No account?</p>
+                            </div>
+
+                             <div class="sign_up_box">   
+                                    <p>No account? Sign up.</p>
+                                    <form action="index.php" method="post" autocomplete="off">
+
+                                    <div class="register-switch">
+                                          <input type="radio" name="gender" value="female" id="sex_f" class="register-switch-input" checked>
+                                          <label for="sex_f" class="register-switch-label">Female</label>
+                                          <input type="radio" name="gender" value="male" id="sex_m" class="register-switch-input">
+                                          <label for="sex_m" class="register-switch-label">Male</label>
+                                    </div>
+                                    <input type="text" name="name" value="" placeholder="Name" pattern="{1,}" required title="Please enter a name."/>
+                                    <!-- The following Attribute 'pattern' is used to validate the input.-->
+                                    <input type="password" name="password" value="" placeholder="Password" pattern="(?=.*\d).{8,}" required title="Must contain at least one number.">
+                                    <input type="password" name="confirm_password" value="" placeholder="Confirm Password"/>
+                                    <!-- The following Attribute 'pattern' is used to validate the input.-->
+                                    <input type="text" name="student_id" value="" placeholder="Student ID" pattern="[0-9].{6,}" required title="7 characters minimum and only numbers are allowed."/>
+                                    <div class="dropdown">    
+                                        <input type="submit" name="signup" value="Sign up!"/> </p> 
+                                    </form>
+                            </div>
+                        </body>
+                    <?php } ?>
 
                     <?php
                         while($row = mysqli_fetch_assoc($result)) { 
