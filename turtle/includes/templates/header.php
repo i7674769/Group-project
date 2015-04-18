7bcdff8a373c28c48c718482b59421afa61aee6d
becky <becky_hughes94@hotmail.co.uk>
2015-04-18 15:11:55 +0100

    some of this is responsive
    This should also include the photos like you asked Darlene

---
+++ turtle/includes/templates/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head> 
+        <title>Project</title>
+        <link href="../../public/css/normal.css" rel="stylesheet">
+        <link href="../../public/css/style.css" rel="stylesheet" type="text/css">
+        
+<!--        <meta http-equiv="Refresh" content="15"><meta http-equiv="Refresh" content="15">-->
+    </head>
+    <body>
---
+++ turtle/public/delete.php
@@ -0,0 +1,17 @@
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php
+    if(isset($_GET["id"])) {
+        $userID = $_GET["id"]; 
+        $query = "DELETE FROM users WHERE id = '{$userID}'";
+        $result = mysqli_query($connect, $query); 
+
+    } else {
+        echo "Problem!";
+    }
+
+    header('Location: logout.php');
+    exit;
+?>
